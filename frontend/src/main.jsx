0b03c124d6a5b4ca06fa9ae4a70024c6c1897385
Ivan Escribano <ivanescribano1998@gmail.com>
2023-05-14 18:11:05 +0200

    vercel analytics testing web page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,14 +5,19 @@ import "./index.css";
 import { Toaster } from "react-hot-toast";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
+
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>{" "}
-      <Toaster position="bottom-right" reverseOrder={false} />
-    </BrowserRouter>
-  </React.StrictMode>
+  <>
+    <Analytics />
+    <React.StrictMode>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>{" "}
+        <Toaster position="bottom-right" reverseOrder={false} />
+      </BrowserRouter>
+    </React.StrictMode>
+  </>
 );
