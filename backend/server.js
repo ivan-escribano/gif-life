6f6d2e6f27d0872bee52b31982b2c54d1324d783
Ivan Escribano <ivanescribano1998@gmail.com>
2023-05-20 19:29:24 +0200

    added custom css to swagger

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 //!ADD ENVIRONMENT VARIABLES
 require("dotenv").config();
 
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+
 //!DEPENDENCIES
 const express = require("express");
 const app = express();
@@ -15,7 +17,7 @@ const connect = require("./config/db");
 connect();
 
 //!MIDDLEWARE
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs, { customCssUrl: CSS_URL }));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(helmet());
